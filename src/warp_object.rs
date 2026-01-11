736a71a7c1465abec0ba95908ed485169348de10
Citeli-py <matheus.citeli2001@gmail.com>
2026-01-11 19:14:52 -0300

    fix: realiza warp tanto para a direita quanto para a esquerda

--- src/warp_object.rs
+++ src/warp_object.rs
@@ -1,13 +1,15 @@
 use crate::types::WORLD_SIZE;
 
+
 pub trait WarpObject {
 
     fn position(&self) -> (f32, f32);
     
     fn warp(&self) -> (f32, f32) {
         let (x, y) = self.position();
-        let warp_x = x%(WORLD_SIZE as f32);
-        let warp_y = y%(WORLD_SIZE as f32);
+
+        let warp_x = if x < 0.0 { WORLD_SIZE as f32 } else { x%(WORLD_SIZE as f32) };
+        let warp_y = if y < 0.0 { WORLD_SIZE as f32 } else { y%(WORLD_SIZE as f32) };
 
         return (warp_x, warp_y);
     }
