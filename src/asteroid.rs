44dd6f3c2481b8de5b87a4b91cd36e84ca879e06
Citeli-py <matheus.citeli2001@gmail.com>
2026-01-21 22:24:01 -0300

    feat: modelo de asteroid

---
+++ src/asteroid.rs
@@ -0,0 +1,63 @@
+use uuid::Uuid;
+
+use crate::collision_object::CollisionObject;
+use crate::warp_object::WarpObject;
+
+#[derive(Clone, Copy)]
+pub enum AsteroidType {
+    SMALL,
+    MEDIUM,
+    BIG
+}
+
+#[derive(Clone)]
+pub struct Asteroid {
+    pub id: Uuid,
+    pub x: f32,
+    pub y: f32,
+    pub radius: u8,
+    pub size: AsteroidType,
+    v: f32,
+    angle: f32
+}
+
+
+impl CollisionObject for Asteroid {
+    fn position(&self) -> (f32, f32) {
+        (self.x, self.y)
+    }
+
+    fn radius(&self) -> f32 {
+        self.radius as f32
+    }
+}
+
+impl WarpObject for Asteroid {
+    fn position(&self) -> (f32, f32) {
+        (self.x, self.y)
+    }
+}
+
+impl Asteroid {
+
+    pub fn new(x: f32, y: f32, size: AsteroidType) -> Asteroid {
+        let r = match size {
+            AsteroidType::BIG => 35,
+            AsteroidType::MEDIUM => 25,
+            AsteroidType::SMALL => 15,
+        };
+
+        Asteroid {id: Uuid::new_v4(), x, y, radius: r, v: 5.0, size: size, angle: rand::random_range(0.0..6.28) }
+    }
+
+    pub fn update(&mut self) {
+        self.x += self.v*f32::cos(self.angle);
+        self.y += self.v*f32::sin(self.angle);
+        
+        (self.x, self.y) = self.warp();
+    }
+
+    pub fn to_json(&self, ) -> String {
+        format!("{{\"id\": \"{}\", \"radius\": \"{}\", \"x\": {}, \"y\": {} }}", self.id, self.radius, self.x, self.y)
+    }
+}
