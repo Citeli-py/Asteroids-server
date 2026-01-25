94b2a3647237f945a2dc9d1c5f1ff81bc687d56f
Citeli-py <matheus.citeli2001@gmail.com>
2026-01-25 13:48:27 -0300

    feat: process info

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,13 @@ mod asteroid_collection;
 mod asteroid;
 mod client;
 
+use std::time::Duration;
 use std::sync::Arc;
 use std::net::SocketAddr;
 use websocket_handler::WebSocketHandler;
 
 use sysinfo::{
-    Components, CpuRefreshKind, Disks, Networks, System
+    System
 };
 
 use axum::{
@@ -32,6 +33,33 @@ async fn health_check() -> (StatusCode, &'static str) {
     println!("Health check!");
     (StatusCode::OK, "OK")
 }
+
+async fn process_info() {
+    let mut sys = System::new_all();
+    let pid = sysinfo::get_current_pid().unwrap();
+
+    // Primeira leitura (CPU precisa de baseline)
+    sys.refresh_processes(sysinfo::ProcessesToUpdate::Some(&[pid]), false);
+
+    loop {
+        // Atualiza apenas o processo atual
+        sys.refresh_processes(sysinfo::ProcessesToUpdate::Some(&[pid]), false);
+
+        if let Some(process) = sys.process(pid) {
+            let ram_mb = process.memory() as f64 / 1024.0;
+            let cpu_usage = process.cpu_usage();
+
+            println!(
+                "Process info: \n\tRAM Used: {:.2} MB \n\tCPU Usage: {:.2}%",
+                ram_mb,
+                cpu_usage
+            );
+        }
+
+        tokio::time::sleep(Duration::from_secs(1)).await;
+    }
+}
+
 
 async fn machine_info() {
     let mut sys = System::new_all();
@@ -80,14 +108,12 @@ async fn machine_info() {
     }
 }
 
-
 #[tokio::main]
 async fn main() {
 
 
-    tokio::spawn(async move {
-        machine_info().await;
-    });
+    tokio::spawn(machine_info());
+    tokio::spawn(process_info());
 
     let server = Arc::new(WebSocketHandler::new());
 
